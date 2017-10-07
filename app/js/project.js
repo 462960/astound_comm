440a2dc6cdf7b3146b25145142707905fb0f4d1c
Vladimir Maxymenko <462960@gmail.com>
2017-10-07 14:05:36 +0300

    Top, gallery completed

--- app/js/project.js
+++ app/js/project.js
@@ -5,57 +5,25 @@ $(function(){
 
 
 const share = document.getElementById('share');
-// const shareClose = document.getElementById('share-close');
-// const socialButtons = document.querySelectorAll('ul.social-center li');
+const galleryImg = document.querySelectorAll('.gallery-wrapper > .responsive');
 
 function shareMe(e){
   e.preventDefault();
-  console.log('Share me!');
   document.getElementById('share-block').classList.toggle('show-block');
 };
 
-function closeShare(){
- document.getElementById('share-block').classList.remove('show-block');
+function removePrice(){
+ const priceTab = this.querySelector('h6.price');
+ priceTab.classList.add('slide-down', 'slide-right');
+};
+
+function showPrice(){
+ const priceTab = this.querySelector('h6.price');
+ priceTab.classList.remove('slide-down', 'slide-right');
 };
 
 share.addEventListener('click', shareMe);
-// shareClose.addEventListener('click', closeShare);
-// socialButtons.forEach(x => x.addEventListener('click', closeShare));
-
-
-
-
-// jQuery staff
-$(function(){
-
-  // $('#share').click(function(e){
-  // 	$('#share-block').toggleClass('show-block');
-  // 	console.log("Share!");
-  // })
-
-// Slide down menu
-$('#burger').click(function(e){
-  e.preventDefault();
-  $('ul.drop-nav').toggleClass('slide-down');
-  $('ul.drop-nav').toggleClass('slide-up');
-});
-// Toggles background of slide down menu
-$('.drop-nav li').click(function(e){
-  e.preventDefault();
-  $('.drop-nav li').removeClass('active');
-  $(this).addClass('active');
-  $('ul.drop-nav').removeClass('slide-down');
-  $('ul.drop-nav').addClass('slide-up');
-});
-
-// Datepicker
-// $('.datepicker').datepick();
-
-// Prevent page jerk
-// $('label').click(function(e){
-//   e.preventDefault();
-//   console.log('Nooooo!')
-//   })
-});
+galleryImg.forEach(x => x.addEventListener('mouseenter', removePrice))
+galleryImg.forEach(x => x.addEventListener('mouseleave', showPrice))
 
 
