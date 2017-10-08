382b2df278773c1d9d77c4ef021904e64f11d32c
Vladimir Maxymenko <462960@gmail.com>
2017-10-08 18:57:25 +0300

    Top, gallery, slider+prices

--- app/js/project.js
+++ app/js/project.js
@@ -6,12 +6,13 @@ $(function(){
 
 const share = document.getElementById('share');
 const galleryImg = document.querySelectorAll('.gallery-wrapper > .responsive');
+const cycleSlides = document.querySelector('.cycle-slideshow');
 
 function shareMe(e){
   e.preventDefault();
   document.getElementById('share-block').classList.toggle('show-block');
 };
-
+// Gallery images 
 function removePrice(){
  const priceTab = this.querySelector('h6.price');
  priceTab.classList.add('slide-down', 'slide-right');
@@ -21,9 +22,21 @@ function showPrice(){
  const priceTab = this.querySelector('h6.price');
  priceTab.classList.remove('slide-down', 'slide-right');
 };
+// Slideshow images
+function priceDown(){
+ const overlay = document.querySelector('.cycle-overlay');
+ overlay.classList.add('price-down');
+};
+
+function priceUp(){
+ const overlay = document.querySelector('.cycle-overlay');
+ overlay.classList.remove('price-down');
+};
 
 share.addEventListener('click', shareMe);
-galleryImg.forEach(x => x.addEventListener('mouseenter', removePrice))
-galleryImg.forEach(x => x.addEventListener('mouseleave', showPrice))
+galleryImg.forEach(x => x.addEventListener('mouseenter', removePrice));
+galleryImg.forEach(x => x.addEventListener('mouseleave', showPrice));
+cycleSlides.addEventListener('mouseenter', priceDown);
+cycleSlides.addEventListener('mouseleave', priceUp);
 
 
